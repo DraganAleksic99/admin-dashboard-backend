49de1d36c1a732c4bb5328529679dc35c236dcea
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-03-05 14:54:25 +0100

    add sale controller

---
+++ src/controllers/saleController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express'
+import Sale from '../models/Sale'
+
+const getSales = async (req: Request, res: Response) => {
+  try {
+    const sales = await Sale.find()
+    res.status(200).json(sales)
+  } catch (e: any) {
+    res.status(400).json({ message: 'Could not retrieve sales' })
+  }
+}
+
+const createSale = async (req: Request, res: Response) => {
+  const sale = req.body
+
+  if (!sale) {
+    return res.status(400).json({ message: 'Sale information missing' })
+  }
+
+  try {
+    await Sale.create(sale)
+    res.status(201).json({ message: 'Sale created' })
+  } catch (e: any) {
+    res.status(400).json({ message: 'Could not create sale' })
+  }
+}
+
+export { getSales, createSale }
