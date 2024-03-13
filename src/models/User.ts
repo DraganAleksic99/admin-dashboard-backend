3dab8795867894efe70709ee0c054622b724de1c
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-03-13 17:50:42 +0100

    update user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,57 @@
 import { Document, Schema, model } from 'mongoose'
 import bcrypt from 'bcryptjs'
 
+type Subscription = {
+  name: string
+  price: number
+  currency: string
+  proposalsLeft: number
+  templatesLeft: number
+  invitesLeft: number
+  adsLeft: number
+  hasAnalytics: boolean
+  hasEmailAlerts: boolean
+}
+
+export type UserType = {
+  _id: string
+  email: string
+  password: string
+  country: string
+  isPublic: boolean
+  phone: string
+  role: string
+  state: string
+  tier: string
+  name: string
+  avatar: string
+  city: string
+  canHire: boolean
+  subscription?: Subscription
+}
+
 export interface IUser extends Document {
+  name: string
   email: string
   password: string
   comparePassword: (enteredPassword: string) => boolean
+  phone: string
+  country?: string
+  isPublic?: boolean
+  role?: string
+  state?: string
+  tier?: string
+  avatar?: string
+  city?: string
+  canHire?: boolean
+  subscription?: Subscription
 }
 
 const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: [true, 'name is required']
+  },
   email: {
     type: String,
     required: [true, 'email is required'],
@@ -16,6 +60,29 @@ const userSchema = new Schema<IUser>({
   password: {
     type: String,
     required: [true, 'password is required']
+  },
+  phone: {
+    type: String,
+    required: [true, 'phone is required']
+  },
+  country: String,
+  isPublic: Boolean,
+  role: String,
+  state: String,
+  tier: String,
+  avatar: String,
+  city: String,
+  canHire: Boolean,
+  subscription: {
+    name: String,
+    price: Number,
+    currency: String,
+    proposalsLeft: Number,
+    templatesLeft: Number,
+    invitesLeft: Number,
+    adsLeft: Number,
+    hasAnalytics: Boolean,
+    hasEmailAlerts: Boolean
   }
 })
 
